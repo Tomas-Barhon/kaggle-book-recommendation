20870a6ce8ac23537905b7dc87a0d1037afebf01
Tomáš Barhoň <tomas.barhon@hotmail.cz>
2024-02-08 00:25:23 +0100

    First item-item version

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.neighbors import NearestNeighbors
-
+import scipy
 
 class Pipeline:
     def __init__(self) -> None:
@@ -36,10 +36,38 @@ class Pipeline:
             categories = categories, nn_model=nn_model)
 
             #remove NAs and compute mean age of these neighbors
-            try:
-                return df.loc[df.userID.isin(neigbours)].dropna().Age.mean()
-            except:
-                #returns overall mean if no neigbors have age
-                return 200000
+            return df.loc[df.userID.isin(neigbours)].dropna().Age.mean()
         else:
             return row.Age
+
+    @staticmethod
+    def create_item_item_similarity_mapping():
+        combined_data = pd.read_csv("./../data/merged_data.csv")
+        combined_data.userID = combined_data.userID.astype("category")
+        combined_data.bookTitle = combined_data.bookTitle.astype("category")
+        users = combined_data["userID"].unique()
+        book_titles = combined_data["bookTitle"].unique()
+        shape = (len(book_titles), len(users))
+        coo = scipy.sparse.coo_matrix((combined_data.bookRating,
+                        (combined_data.bookTitle.cat.codes,
+                         combined_data.userID.cat.codes)),
+                                    shape=shape)
+        csr = coo.tocsr()
+        nn_model = NearestNeighbors(n_neighbors=5, algorithm="auto",
+                            metric="cosine")
+        nn_model.fit(csr)
+        _, nearest_neighbors = nn_model.kneighbors(csr)
+        cat_nearest_books = pd.Categorical.from_codes(nearest_neighbors,
+                                         categories=combined_data.bookTitle.cat.categories)
+        return {title:list(similar.to_numpy()) for title, similar in zip(combined_data.bookTitle.unique(),cat_nearest_books)}
+    @staticmethod
+    def get_item_item_similar(combined_data) -> list:
+        users = combined_data["userID"].unique()
+        book_titles = combined_data["bookTitle"].unique()
+        shape = (len(users), len(book_titles))
+        coo = scipy.sparse.coo_matrix((combined_data.bookRating,
+                        (combined_data.userID.cat.codes,
+                    combined_data.bookTitle.cat.codes)),
+                                    shape=shape)
+        csr = coo.tocsr()
+        
